5cc2ae9918e3f0289f3a8e79c518c9b70463ee8d
github username <github email address>
2020-03-25 11:37:17 -0400

    commit

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,18 +1,4 @@
 class Flight < ActiveRecord::Base
     belongs_to :plane 
-    has_many :passengers  
-    
-    def duration_in_minutes 
-        self.duration_in_minutes 
-    end 
-
-    def origin 
-        self.origin 
-    end 
-
-    
-    def plane 
-        self.planes.map(&:name) 
-    end 
-
+    has_many :passengers 
 end
