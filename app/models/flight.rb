bd0e1481803535eb308597d73060592f3ce8572e
github username <github email address>
2020-03-25 11:08:18 -0400

    commit

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,4 @@
 class Flight < ActiveRecord::Base
     belongs_to :plane 
-    belongs_to :passenger 
+    has_many :passengers  
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,4 @@
 class Passenger < ActiveRecord::Base
-    
+    has_many :planes  
+    has_many :flights, through: :planes 
 end
